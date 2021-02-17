1fa2dffbd9c46bfea98871a3eece629fd08a5da2
Suphakon Jarujunawong <xsoxkill123@gmail.com>
2021-02-17 15:00:32 +0700

    add lab 1.3

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -91,6 +91,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   GPIO_PinState SwitchState[2];//NOW,LAST
   GPIO_PinState SwitchState1[2];
+  GPIO_PinState SwitchState2[2];
   uint16_t LED1_HalfPeriod = 500 ; //1Hz
   uint32_t TimeStamp = 0 ;
   uint32_t ButtonTimeStamp = 0 ;
@@ -175,7 +176,7 @@ int main(void)
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,GPIO_PIN_SET);
 		}
 	}
-
+	//lab1.3
 
   }
   /* USER CODE END 3 */
@@ -279,6 +280,9 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
@@ -305,10 +309,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  /*Configure GPIO pins : PB3 PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
